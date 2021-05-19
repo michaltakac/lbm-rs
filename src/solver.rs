2684be8d65aabac00d0dffc5ff800d7810b5a90a
Michal Takac <takacmichal92@gmail.com>
2021-05-19 07:33:08 +0200

    WIP

--- src/solver.rs
+++ src/solver.rs
@@ -1,234 +1,154 @@
-//! Implements the Lattice-Boltzmann solver.
-
-use num;
-use grid;
-use boundary;
-use io::{vtk, Serializable};
-use time;
-use traits::{Distribution, DistributionStorage};
+use crate::boundary;
+use crate::grid::{StructuredGrid};
+use crate::traits::{Distribution, Physics};
+use crate::FloatNum;
+use arrayfire::*;
+// use time;
 
 /// Lattice-Boltzmann Solver state
-pub struct Solver<P: ::Physics> {
-    grid: grid::StructuredRectangular,
-    pub bcs: boundary::Handler,
+pub struct Solver<P: Physics, D: Distribution> {
+    distribution: D,
+    grid: StructuredGrid<P::Distribution>,
+    pub bcs: boundary::Handler<P::Distribution>,
     physics: P,
-    f: Box<[num]>, // Distribution functions
-    f_hlp: Box<[num]>,
+    f: Array<FloatNum>, // Distribution functions
+    f_hlp: Array<FloatNum>,
 }
 
-impl<P: ::Physics> Solver<P> {
+impl<P: Physics, D: Distribution> Solver<P, D> {
     /// Create a new solver from a `grid` and `physics`.
-    pub fn new(grid: grid::StructuredRectangular, physics: P) -> Solver<P> {
-        Solver {
+    pub fn new(distribution: D, grid: StructuredGrid<D>, bcs: boundary::Handler<D>, physics: P) -> Self {
+        Self {
+            distribution,
             grid,
-            bcs: boundary::Handler::default(),
+            bcs,
             physics,
-            f: vec![0.; grid.size() * P::Distribution::size()]
-                .into_boxed_slice(),
-            f_hlp: vec![0.; grid.size() * P::Distribution::size()]
-                .into_boxed_slice(),
+            f: constant(
+                0.0 as FloatNum,
+                dim4!(D::size() as u64 * P::Distribution::size() as u64),
+            ),
+            f_hlp: constant(
+                0.0 as FloatNum,
+                dim4!(D::size() as u64 * P::Distribution::size() as u64),
+            ),
         }
     }
 
-    /// Initialize distributions functions using `initial_distributions(x)`.
-    pub fn initialize<F>(&mut self, initial_distributions: F)
-    where
-        F: Fn(grid::X)
-            -> DistributionStorage<P::Distribution>,
-    {
-        for c in self.grid.ids() {
-            let fs = initial_distributions(self.grid.x(c));
-            for n in P::Distribution::all() {
-                *self.f_mut(c, n) = fs.as_ref()[n.value()];
-            }
-        }
+    /// Initialize distributions
+    pub fn initialize(&mut self) {
+        // Start in equilibrium state
+        let u_sq: Array<FloatNum> =
+            flat(&(pow(&self.physics.ux, &(2.0 as FloatNum), false) + pow(&&self.physics.uy, &(2.0 as FloatNum), false)));
+        let eu: Array<FloatNum> = flat(
+            &(&mul(&transpose(&self.distribution.ex(), false), &flat(&self.physics.ux), true)
+                + &mul(&transpose(&self.distribution.ey(), false), &flat(&self.physics.uy), true)),
+        );
+
+        self.f = flat(&mul(&transpose(&self.distribution.weights(), false), &flat(&self.physics.density), true))
+            * ((1.0 as FloatNum)
+                + (3.0 as FloatNum) * &eu
+                + (4.5 as FloatNum) * (&pow(&eu, &(2.0 as FloatNum), false))
+                - (1.5 as FloatNum) * (&tile(&flat(&u_sq), dim4!(9))));
+        
+        self.f_hlp = self.f
     }
 
-    /// Index of the distribution function `i` of the cell `c`
-    fn f_idx(c: grid::Idx, i: P::Distribution) -> usize {
-        c.0 * P::Distribution::size() + i.value()
+    /// Mutable reference to the distribution function
+    fn f_mut(&mut self) -> &mut Array<FloatNum> {
+        &mut self.f
     }
 
-    /// Mutable reference to the distribution function `i` of the cell `c`
-    fn f_mut(&mut self, c: grid::Idx, i: P::Distribution) -> &mut num {
-        &mut self.f[Self::f_idx(c, i)]
-    }
-
-    /// Reference to the distribution function `i` of the cell `c`
-    fn f_ref(&self, c: grid::Idx, i: P::Distribution) -> &num {
-        &self.f[Self::f_idx(c, i)]
-    }
-
-    /// Is the cell `c` par of a solid boundary?
-    fn solid_boundary(&self, c: grid::Idx) -> bool {
-        self.bcs.solid_boundary(self.grid.x(c))
+    /// Reference to the distribution function
+    fn f_ref(&self) -> &Array<FloatNum> {
+        &self.f
     }
 
     /// Streaming step
     fn streaming(&mut self) {
-        use rayon::prelude::*;
-        let mut f_hlp =
-            ::std::mem::replace(&mut self.f_hlp, Default::default());
-        f_hlp
-            .par_chunks_mut(P::Distribution::size())
-            .zip(self.grid.par_ids())
-            .for_each(|(f_hlp, c)| for n in P::Distribution::all() {
-                let cn = self.grid.neighbor(c, n);
-                f_hlp[n.value()] = *self.f_ref(cn, n);
-            });
-        self.f_hlp = f_hlp;
+        let mut f_hlp = std::mem::replace(&mut self.f_hlp, self.f.clone());
+        self.f_hlp = view!(f_hlp[P::Distribution::neighbors_index()]);
     }
 
     /// Collision step
     fn collision(&mut self) {
-        let mut f = ::std::mem::replace(&mut self.f, Default::default());
-        use rayon::prelude::*;
-        f.par_chunks_mut(P::Distribution::size())
-            .zip(self.f_hlp.par_chunks(P::Distribution::size()))
-            .zip(self.grid.par_ids())
-            .for_each(|((f, f_hlp), c)| {
-                if self.solid_boundary(c) {
-                    return;
-                }
-                let r = self.physics.collision(&f_hlp, |v, n| v[n.value()]);
-
-                for n in P::Distribution::all() {
-                    f[n.value()] = r.as_ref()[n.value()];
-                }
-            });
-        self.f = f;
+        let mut f = std::mem::replace(&mut self.f, Default::default());
+        
+        self.physics.collision(f, self.grid.dimensions);
     }
 
     /// Applies boundary conditions
     fn apply_boundary_conditions(&mut self) {
-        let mut f = ::std::mem::replace(&mut self.f, Default::default());
-        use rayon::prelude::*;
-        f.par_chunks_mut(P::Distribution::size())
-            .zip(self.grid.par_ids())
-            .for_each(|(f, c)| {
-                let r = self.bcs.apply(
-                    &f,
-                    &self.f_hlp,
-                    |v, n: P::Distribution| v[n.value()],
-                    |v, n| v[Self::f_idx(c, n)],
-                    self.grid.x(c),
-                );
+        let f_hlp = self.f_hlp.copy();
+        let mut f = std::mem::replace(&mut self.f, f_hlp);
 
-                if let Some(r) = r {
-                    for n in P::Distribution::all() {
-                        f[n.value()] = r.as_ref()[n.value()];
-                    }
-                }
-            });
         self.f = f;
+        self.bcs.apply(self.f, self.f_hlp, self.physics);
+        // f.par_chunks_mut(P::Distribution::size())
+        //     .zip(self.grid.par_ids())
+        //     .for_each(|(f, c)| {
+        //         let r = self.bcs.apply(
+        //             &f,
+        //             &self.f_hlp,
+        //             |v, n: P::Distribution| v[n.value()],
+        //             |v, n| v[Self::f_idx(c, n)],
+        //             self.grid.x(c),
+        //         );
+
+        //         if let Some(r) = r {
+        //             for n in P::Distribution::all() {
+        //                 f[n.value()] = r.as_ref()[n.value()];
+        //             }
+        //         }
+        //     });
     }
 
     /// Executes `n_it` iterations writing output every `n_out` iterations.
-    pub fn run(&mut self, n_it: usize, n_out: usize) {
-        let mut n_it = n_it;
-        assert!(n_it > 0);
-        let mut iter = 0;
+    pub fn run(&mut self, iter: usize, write_output: bool) {
         use time::Duration;
-
-        loop {
-            let write_output = n_out > 0 && iter % n_out == 0;
-            let d = Duration::span(|| {
-                let d = Duration::span(|| self.streaming());
-                if write_output {
-                    self.substep("propagation", d);
-                }
-
-                let d = Duration::span(|| self.collision());
-                if write_output {
-                    self.substep("collision", d);
-                }
-
-                let d = Duration::span(|| self.apply_boundary_conditions());
-                if write_output {
-                    self.substep("bcs", d);
-                }
-
-                n_it -= 1;
-                if write_output {
-                    let d = Duration::span(|| self.write_vtk(iter));
-                    self.substep("vtk", d);
-                }
-            });
+        let d = Duration::span(|| {
+            let d = Duration::span(|| self.streaming());
             if write_output {
-                self.step(iter, d);
+                self.substep("streaming", d);
             }
-            if n_it == 0 {
-                break;
-            }
-            iter += 1;
-        }
-    }
 
-    /// Integrates the distribution functions over the volume
-    fn integral(&self) -> num {
-        use rayon::prelude::*;
-        self.grid
-            .par_ids()
-            .map(|c| P::integral(|n| *self.f_ref(c, n)))
-            .sum()
+            let d = Duration::span(|| self.collision());
+            if write_output {
+                self.substep("collision", d);
+            }
+
+            let d = Duration::span(|| self.apply_boundary_conditions());
+            if write_output {
+                self.substep("bcs", d);
+            }
+
+            // if write_output {
+            //     let d = Duration::span(|| self.visualize());
+            //     self.substep("visualize", d);
+            // }
+        });
+        if write_output {
+            self.step(iter, d);
+        }
     }
 
     /// Prints line info of a whole iteration step
     fn step(&self, n_it: usize, duration: time::Duration) {
-        let integral = self.integral();
         println!(
-            "#{} | integral: {} | duration: {} ms",
+            "#{} | duration: {} ms",
             n_it,
-            integral,
             duration.num_milliseconds()
         );
     }
     /// Prints line info of an iteration sub-step
     fn substep(&self, name: &str, duration: time::Duration) {
-        let res = self.integral();
         println!(
-            "# [{}] | integral: {} | duration: {} \u{03BC}s",
+            "# [{}] | duration: {} \u{03BC}s",
             name,
-            res,
             duration.num_microseconds().unwrap()
         );
     }
-
     /// Writes the solution to a VTK file.
-    fn write_vtk(&self, n_it: usize) {
-        let fname = format!("lbm_rs_output_{}", n_it);
-        let mut vtk_writer = vtk::write_vtk(&fname, self.grid);
-        vtk_writer = self.physics.write(
-            vtk_writer,
-            |c| self.solid_boundary(c),
-            |c, n| *self.f_ref(c, n),
-        );
-        vtk_writer.write_scalar("boundary_idx", |c| {
-            self.bcs.idx(self.grid.x(c)).map_or(-1 as i32, |v| v as i32)
-        });
+    fn visualize(&self, iter: usize) {
+        unimplemented!();
     }
 }
-
-/*
-impl<P: ::Physics> Serializable for Solver<P> {
-    type CellIndex = grid::Idx;
-    type CellGeometry = geometry::Square;
-    type CellIndexIterator = _;
-    fn cells(&self) -> {
-        self.grid.ids()
-    }
-    fn geometry(&self, c: Self::CellIndex) -> Self::CellGeometry {
-        self.grid.geometry(c)
-    }
-    fn cell_data<T: CellDataWriter<Self::CellIndex>>(&self, writer: &mut T) {
-        self.physics.write(
-            writer,
-            |c| self.solid_boundary(c),
-            |c, n| *self.f_ref(c, n),
-        );
-        writer.write_scalar("boundary_idx", |c| {
-            self.bcs.idx(self.grid.x(c)).map_or(-1 as i32, |v| v as i32)
-        });
-    }
-}
-*/
